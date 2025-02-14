d251604fbb3e4db3b17527ace7743008766b10f1
FernandoM <m87.fernando@gmail.com>
2025-02-13 21:03:23 -0400

    fix: path

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const { SystemQueryBuilder } = require("../odata/system-query-builder.model");
-const { Restrictions } = require("../odata/restrictions.factory"); 
+const { SystemQueryBuilder } = require("./odata/system-query-builder.model");
+const { Restrictions } = require("./odata/restrictions.factory"); 
 
 module.exports = { 
     SystemQueryBuilder,
